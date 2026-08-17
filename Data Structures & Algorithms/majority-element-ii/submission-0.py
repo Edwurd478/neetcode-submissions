c6fb164476212b69cfe76233a15724bbc104ed61
Edward <66580141+Edwurd478@users.noreply.github.com>
2026-08-17 02:08:45 -0500

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,10 @@
+from collections import defaultdict, Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        counter = Counter(nums)
+        ans = []
+        for key, val in counter.items():
+            if counter[key] > len(nums) // 3:
+                ans.append(key)
+        
+        return ans
