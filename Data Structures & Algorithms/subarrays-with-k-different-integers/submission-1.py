febbba4808d0b73709f3481727728bf739bb69f2
Edward <66580141+Edwurd478@users.noreply.github.com>
2026-09-07 20:54:47 -0500

    Add: subarrays-with-k-different-integers - submission-1

---
+++ Data Structures & Algorithms/subarrays-with-k-different-integers/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        
+        def at_most(n: int) -> int:
+            freq = {}
+            count = 0
+            l = 0
+
+            for r in range(len(nums)):
+                freq[nums[r]] = freq.get(nums[r], 0) + 1
+
+                while len(freq) > n:
+                    freq[nums[l]] -= 1
+                    if freq[nums[l]] == 0:
+                        del freq[nums[l]]
+                    l += 1
+                
+                count += r - l + 1
+            return count
+        
+        return at_most(k) - at_most(k-1)
+        
